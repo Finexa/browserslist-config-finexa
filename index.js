6562ecc191b6cece16c353a8984face933e9dde0
Felix Schoeler <felix.schoeler@finexa.no>
2019-03-20 10:09:55 +0100

    Added browserslist config

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = [
+  'last 2 Chrome versions',
+  'last 2 Firefox versions',
+  'last 2 Safari versions',
+  'last 2 Edge versions',
+  'last 3 ChromeAndroid versions',
+  'last 2 iOS versions',
+];
